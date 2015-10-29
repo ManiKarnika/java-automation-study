d3c1997e22c5dcd810b007fea3e7a4a3d86b5558
Dmitry Skvortsov <dscwortsov@gmail.com>
2015-10-28 19:24:01 -0700

    Д37
    Some additional RandomGenerators

--- projests/src/com/example/tests/TestBase.java
+++ projests/src/com/example/tests/TestBase.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -42,32 +43,33 @@ public class TestBase {
 	  
 	  public String generateRandomString() {
 		  Random rnd = new Random();
+		  
 		  if (rnd.nextInt(5) == 0) {
 			   return "";
 		   } else {
-			   return "test" + rnd.nextInt();
+			   return RandomStringUtils.randomAlphabetic(4) + rnd.nextInt();
 		   }
 	  }
 	  
 	  @DataProvider	
 	  public Iterator<Object[]> randomValidContactsGenerator() {
-		  List<Object[]> list = new ArrayList<Object[]>();  
-		  for (int i = 0; i < 5; i++) {
+		  List<Object[]> list = new ArrayList<Object[]>();  		  
+		  for (int i = 0; i < 5; i++) {  
 			ContactData contact = new ContactData();
 			contact.contactLastName = generateRandomString();
 			contact.contactFirstName = generateRandomString();
-			contact.contactFistAddress = "3555 Alabama St,\nPortland, 97202\nUSA";
-			contact.contactHomePhone = "+1(503)231-5657";
-			contact.contactMobilePhone = "+1(503)231-5658";
-			contact.contactWorkPhone = "+1(503)231-5659";
-			contact.contactFirstEmail = "sc@sc.com";
-			contact.contactSecondEmail = "sc1@sc.com";
-			contact.contactBirthDay = "1";
+			contact.contactFistAddress = generateRandomAddress();
+			contact.contactHomePhone = generateRandomPhoneNumber();
+			contact.contactMobilePhone = generateRandomPhoneNumber();
+			contact.contactWorkPhone = generateRandomPhoneNumber();
+			contact.contactFirstEmail = generateRandomEmail();
+			contact.contactSecondEmail = generateRandomEmail();
+			contact.contactBirthDay = "31";
 			contact.contactBirthMonth = "January";
-			contact.contactBirthYear = "1980";
+			contact.contactBirthYear = "1990";
 			contact.contactGroup = "tempo";
-			contact.contactSecondAddress = "3556 Alabama St,\nPortland, 97202\nUSA";
-			contact.contactSecondAddressPhone = "+1(503)231-5659";
+			contact.contactSecondAddress = generateRandomAddress();
+			contact.contactSecondAddressPhone = generateRandomPhoneNumber();
 			list.add(new Object[]{contact});
 		}
 		  return list.iterator();
@@ -78,9 +80,24 @@ public class TestBase {
 			if (rnd.nextInt(6) == 0) {
 				return "";
 			} else {
-				return "contact" + rnd.nextInt();
+				return RandomStringUtils.randomAlphabetic(4) + rnd.nextInt();
 			}
 
 	  } 
+	  
+	  public String generateRandomPhoneNumber() {
+		  return "+" + RandomStringUtils.randomNumeric(1) + "(" + RandomStringUtils.randomNumeric(3) + ")" 
+				  		+ RandomStringUtils.randomNumeric(6);
+	  }
+	  
+	  public String generateRandomEmail() {
+		  return RandomStringUtils.randomAlphabetic(5) + "@" + RandomStringUtils.randomAlphabetic(5) 
+		  				+ "." + RandomStringUtils.randomAlphabetic(3);
+	  }
+	  
+	  public String generateRandomAddress() {
+		  return RandomStringUtils.randomAlphabetic(5) + "\n" + RandomStringUtils.randomAlphabetic(5) 
+		  				+ "\n" + RandomStringUtils.randomAlphabetic(3);
+	  }
 
 }
